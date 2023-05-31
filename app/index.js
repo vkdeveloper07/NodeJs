58b3e1602705cab11cfac073ccee98d8ea3019a9
vishalthedeveloper07@gmail.com <vishalthedeveloper07@gmail.com>
2023-05-31 21:43:24 +0530

    section 3.8 parsing payload

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@
 //Dependencies
 const http = require('http');
 const url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 const port = 3000;
 const server = http.createServer(function(req, res){
 
@@ -21,13 +22,25 @@ const server = http.createServer(function(req, res){
     //Send the response
     const trimmedPath = path.replace(/^\/+|\/+$/g,'')
     //Log the request path
-    res.end('Hello World\n');
-    console.log(`request received on : ${trimmedPath} with this ${method} method `);
-    console.log('and with this queryString parameter ', queryStringObject);
+
 
     //get the headers
     const headers = req.headers;
     console.log(headers);
+
+    //get the payload  section 3.8 parsing payload
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    });
+    req.on('end', function(){
+        buffer += decoder.end();
+        res.end('Hello World\n');
+        console.log(`request received on : ${trimmedPath} with this ${method} method `);
+        console.log('req received with payload', buffer);
+    });
+
 });
 
 server.listen(port,() => {
